9a00f503d724bed78c5b4c2122bb3de0d888c5f4
cjyueh <cjyueh@gmail.com>
2016-04-01 12:16:23 -0700

    user can add comment

--- flapper-news/app/assets/javascripts/posts/postsCtrl.js
+++ flapper-news/app/assets/javascripts/posts/postsCtrl.js
@@ -5,11 +5,17 @@ angular.module('flapperNews')
       // $scope.post = posts.posts[$stateParams.id]; //grat a post from posts using id from $stateParams
       $scope.addComment = function() {
         if($scope.body === '') { return; }
-        $scope.post.comments.push({
+        posts.addComment(post.id, {
           body: $scope.body,
-          author: 'user',
-          upvotes: 0
+          author: 'user'
+        }).success(function(comment) {
+          $scope.post.comments.push(comment);
         });
+        // $scope.post.comments.push({
+        //   body: $scope.body,
+        //   author: 'user',
+        //   upvotes: 0
+        // });
         $scope.body = '';
       };
     }]);
