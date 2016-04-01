448492958ee076161ba00aaf8a4e1c88e1be576b
cjyueh <cjyueh@gmail.com>
2016-04-01 12:05:21 -0700

    user can upvote posts

--- flapper-news/app/assets/javascripts/home/mainCtrl.js
+++ flapper-news/app/assets/javascripts/home/mainCtrl.js
@@ -12,6 +12,6 @@ angular.module('flapperNews')
       $scope.link = '';
     };
     $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
+      posts.upvote(post);
     };
   }]);
--- flapper-news/app/assets/javascripts/posts/posts.js
+++ flapper-news/app/assets/javascripts/posts/posts.js
@@ -13,5 +13,10 @@ angular.module('flapperNews')
         o.posts.push(data);
       });
     };
+    o.upvote = function(post) {
+      return $http.put('/posts/' + post.id + '/upvote.json').success(function(data) {
+        post.upvotes += 1;
+      });
+    };
     return o;
   }]);
