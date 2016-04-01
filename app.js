c93d9076d8ea2b7877b5584139a244b20b8e4b3a
cjyueh <cjyueh@gmail.com>
2016-03-31 22:07:14 -0700

    creates add comment function and form

--- app.js
+++ app.js
@@ -46,4 +46,13 @@ angular.module('flapperNews', ['ui.router'])
   .controller('PostsCtrl', [
     '$scope', '$stateParams', 'posts', function($scope, $stateParams, posts) {
       $scope.post = posts.posts[$stateParams.id]; //grat a post from posts using id from $stateParams
+      $scope.addComment = function() {
+        if($scope.body === '') { return; }
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body = '';
+      };
     }]);
